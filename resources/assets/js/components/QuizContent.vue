e467337de8314ca4f2f43f686bca21a83a7b8902
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-15 16:06:22 -0500

    Default (stub) Vue Component for taking the Quiz. Ref #3

---
+++ resources/assets/js/components/QuizContent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="content">
+        Test!
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
