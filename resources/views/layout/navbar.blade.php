ab78d5a157e68e07b7f6a483ddec55ee9efc9b89
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-15 16:01:29 -0500

    Add official Tweeter link

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,11 +3,6 @@
         <a class="navbar-item" href="{{ url('/') }}">
             <img src="{{ asset('img/quizknows.png') }}" alt="Quiz Knows Logo" width="225" height="50">
         </a>
-        <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
     </div>
 
     <div id="navbar" class="navbar-menu">
@@ -17,7 +12,7 @@
                 <div class="field is-grouped">
                     <p class="control">
                         <a class="bd-tw-button button is-info" data-social-network="Twitter" data-social-action="tweet"
-                           target="_blank" href="">
+                           target="_blank" href="https://twitter.com/Quiz_Knows">
                             <span class="icon">
                                 <i class="fab fa-twitter"></i>
                             </span>
