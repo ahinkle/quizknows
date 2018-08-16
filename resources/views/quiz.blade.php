178c86d4af466c44ce50f616f40b3ace8beed64d
DaltonMcCleery <dmccleery@apsolutions.net>
2018-08-16 10:01:30 -0500

    Remove Question title from blade and put into Vue component. Ref #3

--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -5,7 +5,6 @@
         <div class="tile is-parent">
             <article class="tile is-child notification is-info">
                 <div class="content">
-                    <p class="title has-text-centered">Title</p>
                     <quiz-content></quiz-content>
                 </div>
             </article>
