075f9e65f74898d45c636310867d88e60d631922
ahinkle <ahinkle10@gmail.com>
2018-08-16 09:20:26 -0500

    Update the restraunt weights, based upon the users selection.

--- app/Http/Controllers/Api/QuizController.php
+++ app/Http/Controllers/Api/QuizController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Restaurant;
+use App\Answer;
 
 class QuizController extends Controller
 {
@@ -21,6 +22,22 @@ class QuizController extends Controller
         foreach ($restaurants as $restaurant) {
             $request->session(['restaurant_weight['.$restaurant->id.']' => 0]);
         }
+        return $request;
+    }
+
+    /**
+     * Update the restraunt weights, based upon the users selection.
+     *
+     * @param  Request  $Request
+     * @param  Answer   $answer
+     * @return Request
+     */
+    public function update(Answer $answer, Request $request)
+    {
+        foreach ($answer->restaurant_weights as $key => $restaurantWeight) {
+            $request->session(['restaurant_weight[' . $key. ']' => 'restaurant_weight[' . $key. ']' + $restaurantWeight['weight']]);
+        }
+        
         return $request;
     }
 }
